349eeccfad4233d51c08f7504bf5ed48eab82ee1
Sandeep Raju Prabhakar <me@sandeepraju.in>
2016-05-27 20:52:54 -0500

    adds ssh config

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install SSH config
+echo "installing ssh config file"
+mkdir -p ~/.ssh/
+cp ssh/config ~/.ssh/
+
 # Install terminator config
 echo "installing terminator config file"
 mkdir -p ~/.config/terminator/
